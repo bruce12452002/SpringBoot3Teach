f784f6accb9b36a6c34cf8389df999a8400898a5
bruceChen <bruce@bruceChendeMacBook-Pro.local>
2023-06-17 14:11:49 +0800

    add i18n

--- src/main/java/home/bruce/SpringBoot3Teach/controller/MyController.java
+++ src/main/java/home/bruce/SpringBoot3Teach/controller/MyController.java
@@ -1,16 +1,24 @@
 package home.bruce.SpringBoot3Teach.controller;
 
 import home.bruce.SpringBoot3Teach.bean.WuKon;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @Slf4j  // 使用了 lombok 後，可以用這個註解，然後自然有個 log 變數可用
 @RestController
 public class MyController {
     Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello")
     public String hello() {
@@ -30,5 +38,23 @@ public class MyController {
     @GetMapping("/negotiation")
     public WuKon negotiation() {
         return new WuKon();
+    }
+
+    /**
+     * No message found under code 'key' for locale 'zh_TW'
+     * 一定要有不帶國碼的 xxx.properties
+     */
+    @GetMapping("/i18n")
+    public void i18n(HttpServletRequest request) {
+        Locale locale = request.getLocale();
+        System.out.println(messageSource.getMessage("msg", new String[]{"龍", "測試"}, locale));
+
+        String cn = messageSource.getMessage("msg", new String[]{"龙", "测试", "3"}, Locale.CHINA);
+        String tw = messageSource.getMessage("msg", new String[]{"龍", "測試"}, Locale.TAIWAN);
+        String us = messageSource.getMessage("msg", new String[]{"dragon"}, Locale.ENGLISH);
+
+        System.out.println("cn=" + cn);
+        System.out.println("tw=" + tw);
+        System.out.println("us=" + us);
     }
 }
